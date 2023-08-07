79e306d72a09d257cf99514b6939e8d989ebaabf
mohameddawo0od <126067254+mohameddawo0od@users.noreply.github.com>
2023-08-07 14:33:25 +0400

    Update HelloController.java

--- src/main/java/io/datawire/labs/hellospring/HelloController.java
+++ src/main/java/io/datawire/labs/hellospring/HelloController.java
@@ -13,12 +13,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        long millis = System.currentTimeMillis() - start;
-        String uptime = String.format("%02d:%02d",
-                                      TimeUnit.MILLISECONDS.toMinutes(millis),
-                                      TimeUnit.MILLISECONDS.toSeconds(millis) -
-                                      TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-        return String.format("Hello, Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
+        return "Greetings from Spring Boot!";
     }
 
 }
