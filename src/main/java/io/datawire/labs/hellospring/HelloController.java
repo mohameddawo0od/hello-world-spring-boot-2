188651b81b8cb80bed16ed2dddde2559f59d96bd
mohameddawo0od <126067254+mohameddawo0od@users.noreply.github.com>
2023-08-07 14:38:19 +0400

    Update HelloController.java

--- src/main/java/io/datawire/labs/hellospring/HelloController.java
+++ src/main/java/io/datawire/labs/hellospring/HelloController.java
@@ -5,13 +5,14 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
     private static long start = System.currentTimeMillis();
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String sayHello() {
         return "Greetings from Spring Boot!";
     }
